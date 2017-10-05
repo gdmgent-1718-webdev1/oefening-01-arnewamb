ce9520b4149b200dc9571e34447d576a30141412
arnewamb <arne.wambeke@student.arteveldehs.be>
2017-10-05 09:20:13 +0200

    some

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$string1 = "hello";
+$string2 = "world";
+
+echo $string1, $string2;
+
+$x = 7;
+$y = 10;
+
+echo $x + $y;
+echo $x - $y;
+echo $x * $y;
+echo $x / $y;
+echo $x % $y;
+
+?>
